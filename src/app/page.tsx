4845a71377e989d179df2d0e3d2d38eadd789eb1
Suresh Thapa <zorosenpai456@gmail.com>
2025-02-22 15:30:52 +0545

    prisma added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function page() {
+import prisma from "./lib/prisma";
+
+export default async function page() {
+  const users = await prisma.user.findMany();
+
+  console.log(users);
   return <div>page</div>;
 }
