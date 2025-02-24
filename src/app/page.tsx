e529f47b92ab4ebc10cf49bc7a144dd8037a86bb
Suresh Thapa <zorosenpai456@gmail.com>
2025-02-24 18:38:14 +0545

    page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function page() {
-  return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  );
+  return <div>home page</div>;
 }
